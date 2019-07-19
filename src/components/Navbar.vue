6df00b3a8ccb10789af661f0f2fc93bcad3a28a5
knifec <ckxgoodzc@gmail.com>
2019-07-19 21:00:29 +0800

    砍了小组功能

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,7 +8,7 @@
       <el-menu-item index="/question">题目</el-menu-item>
       <el-menu-item :index="questionbanklink()" v-if="user.userType!==''">个人题库</el-menu-item>
       <el-menu-item index="/exam" v-if="user.userType!==''">考试中心</el-menu-item>
-      <el-menu-item index="/group" v-if="user.userType!==''">你的小组</el-menu-item>
+      <!-- <el-menu-item index="/group" v-if="user.userType!==''">你的小组</el-menu-item> -->
       <el-menu-item index="/teacher" v-if="user.userType==='teacher'">教师中心</el-menu-item>
       <el-menu-item index="/admin" v-if="user.userType==='admin'">管理员面板</el-menu-item>
       <!-- <el-submenu index="2">
